389b3e0cd2ed98c389890ed1a421693abffacc18
Danny Koppenhagen <mail@d-koppenhagen.de>
2021-12-12 20:53:06 +0100

    fix: optimize tailwind build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    // enabled: process.env.TAILWIND_MODE === 'build',
+    content: ['./src/**/*.{html,ts}'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
